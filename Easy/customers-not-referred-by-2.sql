32794824b6aa466366fa92a33591bb9a1e7e7352
Meenakshi Rajpurohit <122378872+meenakshi-12345@users.noreply.github.com>
2026-05-07 20:33:21 +0530

    Add files via upload

--- Easy/customers-not-referred-by-2.sql
+++ Easy/customers-not-referred-by-2.sql
@@ -1,11 +1,14 @@
--- Problem: Customers Not Referred by ID 2
--- Platform: LeetCode 
+-- Problem: Find Customers Not Referred by ID 2
+-- platform: Leetcode
 -- Difficulty: Easy
 -- Description:
--- Retrieve the names of customers whose referee_id is not equal to 2.
--- Include customers whose referee_id is NULL.
+-- Retrieve the names of customers who:
+-- 1. Were not referred by customer with id = 2
+-- 2. Or have no referee_id (NULL)
 
-SELECT name
-FROM customer
-WHERE referee_id != 2
-   OR referee_id IS NULL;
+SELECT 
+    name
+FROM Customer
+WHERE 
+    referee_id != 2 
+    OR referee_id IS NULL;
